001ac182625be1cacd4b5bdc85d753c4ad6448d9
shiincs <skywing27@gmail.com>
2019-04-19 17:30:08 +0900

    feat: hamburger menu toggle interaction

--- src/components/LayoutView/LayoutView.jsx
+++ src/components/LayoutView/LayoutView.jsx
@@ -5,31 +5,38 @@ import styles from './LayoutView.scss';
 
 const cx = classNames.bind(styles);
 
-const LayoutView = () => {
+const LayoutView = props => {
+  const { isNavMenuOpen, handleNavMenu } = props;
+
   return (
-    <header className={cx('header')}>
-      <h1 className={cx('appTitle')}>With My Dog</h1>
-      <div className={cx('navButton')}>
-        <a href="#" className={cx('btn')}>
-          메뉴 버튼
-        </a>
-      </div>
-      <nav className={cx('navMenu')}>
-        <ul className={cx('navList')}>
-          <li className={cx('navItem')}>내 참여</li>
-          <li className={cx('navItem')}>내 장소</li>
-          <li className={cx('navItem')}>도움말</li>
-          <li className={cx('navItem')}>의견 보내기</li>
-          <li className={cx('navItem')}>설정</li>
-          <li className={cx('navItem')}>로그아웃</li>
-        </ul>
-      </nav>
-      <div className={cx('addPlaceButton')}>
-        <a href="#" className={cx('btn')}>
-          장소 등록하기
-        </a>
-      </div>
-    </header>
+    <>
+      <header className={cx('header')}>
+        <h1 className={cx('appTitle')}>With My Dog</h1>
+        <div className={cx('navButton')}>
+          <a className={cx('btn')} onClick={() => handleNavMenu()}>
+            햄버거메뉴
+          </a>
+        </div>
+        <nav className={cx('navMenu', { open: isNavMenuOpen })}>
+          <ul className={cx('navList')}>
+            <li className={cx('navItem')}>내 참여</li>
+            <li className={cx('navItem')}>내 장소</li>
+            <li className={cx('navItem')}>도움말</li>
+            <li className={cx('navItem')}>의견 보내기</li>
+            <li className={cx('navItem')}>설정</li>
+            <li className={cx('navItem')}>로그아웃</li>
+          </ul>
+          <a onClick={() => handleNavMenu()}>닫기</a>
+        </nav>
+        <div className={cx('addPlaceButton')}>
+          <a className={cx('btn')}>장소 등록하기</a>
+        </div>
+      </header>
+      <div
+        className={cx('navMenuOverlay', { open: isNavMenuOpen })}
+        onClick={isNavMenuOpen ? () => handleNavMenu() : null}
+      />
+    </>
   );
 };
 
--- src/containers/LayoutContainer.jsx
+++ src/containers/LayoutContainer.jsx
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import LayoutView from '../components/LayoutView/LayoutView';
 
 export default class LayoutContainer extends Component {
+  state = {
+    isNavMenuOpen: false,
+  };
+
+  handleNavMenu() {
+    this.setState(prev => ({
+      isNavMenuOpen: !prev.isNavMenuOpen,
+    }));
+  }
+
   render() {
     const { children } = this.props;
+    const { isNavMenuOpen } = this.state;
+
     return (
       <>
-        <LayoutView />
+        <LayoutView
+          isNavMenuOpen={isNavMenuOpen}
+          handleNavMenu={() => this.handleNavMenu()}
+        />
         {children}
       </>
     );
