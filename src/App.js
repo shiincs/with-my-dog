aaa94565cd0569e402c0f8ea631a8f5e0b926038
shiincs <skywing27@gmail.com>
2019-04-19 17:21:58 +0900

    feat: add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import store from './store';
 
 import './App.scss';
@@ -9,7 +11,11 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <MainContainer />
+        <Router>
+          {/* <MainContainer /> */}
+          <Route path="/:category" component={MainContainer} />
+          <Route exact path="/" component={MainContainer} />
+        </Router>
       </Provider>
     );
   }
