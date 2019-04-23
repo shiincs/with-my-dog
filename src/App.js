1d2604b6ecc07b26d0eb515c586b5e79dc791c93
shiincs <skywing27@gmail.com>
2019-04-23 20:52:20 +0900

    feat: add MapProvider for use context api

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import store from './store';
 import './App.scss';
 import MainContainer from './containers/MainContainer';
 
+import { MapProvider } from './contexts/MapContext';
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
-          {/* <MainContainer /> */}
-          <Route path="/:category" component={MainContainer} />
-          <Route exact path="/" component={MainContainer} />
-        </Router>
+        <MapProvider>
+          <Router>
+            {/* <MainContainer /> */}
+            <Route path="/:category" component={MainContainer} />
+            <Route exact path="/" component={MainContainer} />
+          </Router>
+        </MapProvider>
       </Provider>
     );
   }
