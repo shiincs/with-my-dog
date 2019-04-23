2a3e41582ebb54d045aa5bc885e2cf7ff1c419aa
shiincs <skywing27@gmail.com>
2019-04-23 12:20:07 +0900

    fix: edit condition about create store on production env

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,23 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../ducks';
 
 const middlewares = [thunk];
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(...middlewares)
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+let store;
+
+if (process.env.NODE_ENV !== 'production') {
+  store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(...middlewares),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = createStore(rootReducer, applyMiddleware(...middlewares));
+}
 
 export default store;
