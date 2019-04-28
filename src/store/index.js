d7435283610ddf54e1e7b155d79d8b27ca58a4cc
shiincs <skywing27@gmail.com>
2019-04-28 12:12:41 +0900

    fix: remove compose

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,23 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from '../ducks';
 
 const middlewares = [thunk];
 
-let store;
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-if (process.env.NODE_ENV !== 'production') {
-  store = createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(...middlewares),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-} else {
-  store = createStore(rootReducer, applyMiddleware(...middlewares));
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   store = createStore(
+//     rootReducer,
+//     compose(
+//       applyMiddleware(...middlewares),
+//       window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//         window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+//   );
+// } else {
+
+// }
 
 export default store;
